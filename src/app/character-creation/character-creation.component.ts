3ef727fcb7bd124e0d9480a627e7683447a51209
Daniel Scholler <shpaackle@gmail.com>
2021-08-05 22:48:49 -0400

    removed creation-step & step-selections components

--- src/app/character-creation/character-creation.component.ts
+++ src/app/character-creation/character-creation.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Die } from '../shared/die.model';
-import { CreationStep } from './creation-step/creation-step.model';
+import { CreationStep } from './creation-step.model';
 
 @Component({
   selector: 'app-character-creation',
   templateUrl: './character-creation.component.html',
-  styleUrls: ['./character-creation.component.css']
+  styleUrls: ['./character-creation.component.css'],
 })
 export class CharacterCreationComponent implements OnInit {
   creationSteps: CreationStep[] = [];
@@ -17,19 +17,27 @@ export class CharacterCreationComponent implements OnInit {
     const d4 = new Die(4);
 
     // create placeholder text for now
-    const firstStep = new CreationStep(1, "Background Selection", "This is the first step", [d10, d10])
-    const secondStep = new CreationStep(2, "Second Step", "This is the second step", [d4, d4])
+    const firstStep = new CreationStep(
+      1,
+      'Background Selection',
+      'This is the first step',
+      [d10, d10]
+    );
+    const secondStep = new CreationStep(
+      2,
+      'Second Step',
+      'This is the second step',
+      [d4, d4]
+    );
 
     // push placeholder text into list of steps
     this.creationSteps.push(firstStep);
     this.creationSteps.push(secondStep);
-   }
-
-  ngOnInit(): void {
   }
+
+  ngOnInit(): void {}
 
   onStepSelect(step: number) {
     this.currentStep.emit(step);
   }
-
 }
