b810e2a7e184d47fc83f696925b5d3f15e5a9b61
Daniel Scholler <shpaackle@gmail.com>
2021-08-07 21:15:34 -0400

    added backgroundText property

--- src/app/shared/background.model.ts
+++ src/app/shared/background.model.ts
@@ -1,9 +1,10 @@
 import { Die } from './die.model';
 
-export class Background {
+export class CharacterBackground {
   constructor(
     public number: number,
     public name: string,
+    public backgroundText: string,
     public qualities: string[],
     public principleCategory: string,
     public powerSourceDice: Die[],
