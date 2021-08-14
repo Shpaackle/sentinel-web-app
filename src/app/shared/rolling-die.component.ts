3757096f84b8a5e2f2bd6530b89ae7712030f5f1
Daniel Scholler <shpaackle@gmail.com>
2021-08-14 00:10:03 -0400

    added rolling-die component

---
+++ src/app/shared/rolling-die.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, } from '@angular/core';
+import { Die, } from './die.model';
+
+@Component({
+    selector: 'app-rolling-die',
+    templateUrl: 'rolling-die.component.html',
+},)
+export class RollingDieComponent {
+    @Input() die: Die;
+}
