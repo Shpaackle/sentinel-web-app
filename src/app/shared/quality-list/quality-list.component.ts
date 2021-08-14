f42f915b72d588849b0626af56e377987a22ae22
Daniel Scholler <shpaackle@gmail.com>
2021-08-14 16:49:56 -0400

    added populateOptionsList

--- src/app/shared/quality-list/quality-list.component.ts
+++ src/app/shared/quality-list/quality-list.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Die } from '../die.model';
 
 @Component({
-  selector: 'app-quality-list',
-  templateUrl: './quality-list.component.html',
-  styleUrls: ['./quality-list.component.css']
+    selector: 'app-quality-list',
+    templateUrl: './quality-list.component.html',
+    styleUrls: ['./quality-list.component.css'],
 })
 export class QualityListComponent implements OnInit {
+    options = [];
+    @Input() jsonData: string[] = [];
+    @Input() assignedDie: Die;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.options = [];
+    }
 
+    populateOptionsList() {
+        for (let optionName of this.jsonData) {
+            this.options.push(optionName);
+            switch (optionName) {
+            case 'any Mental':
+                this.options.push('Alertness');
+                this.options.push('Conviction');
+                this.options.push('Creativity');
+                this.options.push('Investigation');
+                this.options.push('Self-Discipline');
+                break;
+            case 'any Physical':
+                this.options.push('Acrobatics');
+                this.options.push('Close Combat');
+                this.options.push('Finesse');
+                this.options.push('Fitness');
+                this.options.push('Ranged Combat');
+                this.options.push('Stealth');
+                break;
+            case 'any Social':
+                this.options.push('Banter');
+                this.options.push('Insight');
+                this.options.push('Imposing');
+                this.options.push('Leadership');
+                this.options.push('Persuasion');
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
 }
