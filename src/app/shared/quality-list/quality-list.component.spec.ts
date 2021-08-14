2952946cbd953ab66b1f1aa3153f056acc6f2f86
Daniel Scholler <shpaackle@gmail.com>
2021-08-14 16:49:25 -0400

    whitespace differences

--- src/app/shared/quality-list/quality-list.component.spec.ts
+++ src/app/shared/quality-list/quality-list.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { QualityListComponent } from './quality-list.component';
 
 describe('QualityListComponent', () => {
-  let component: QualityListComponent;
-  let fixture: ComponentFixture<QualityListComponent>;
+    let component: QualityListComponent;
+    let fixture: ComponentFixture<QualityListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ QualityListComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ QualityListComponent ]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(QualityListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(QualityListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
