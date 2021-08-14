531ad5c06fba9ddcdccb7f7c9ea38c6c6c144437
Daniel Scholler <shpaackle@gmail.com>
2021-08-14 12:00:33 -0400

    changeg die images to png for transp. background

--- src/app/shared/die.model.ts
+++ src/app/shared/die.model.ts
@@ -1,11 +1,11 @@
 export class Die {
-  public sides: number;
-  public imagePath: string;
-  public faceShowing: number;
+    public sides: number;
+    public imagePath: string;
+    public faceShowing: number;
 
-  constructor(sides: number = 4,) {
-      this.sides = sides;
-      this.faceShowing = sides;
-      this.imagePath = './assets/dice-images/D' + sides + '.jpg';
-  }
+    constructor(sides: number = 4) {
+        this.sides = sides;
+        this.faceShowing = sides;
+        this.imagePath = './assets/dice-images/D' + sides + '.png';
+    }
 }
