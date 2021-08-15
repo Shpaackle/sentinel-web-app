67c3bee916141d767bac5b9dd5baa6569e357cfd
Daniel Scholler <shpaackle@gmail.com>
2021-08-15 13:55:21 -0400

    -added ICharacterBackground interface -changed property variables accordingly -used Object.assign in constructor -changed for loops to use array.map instead

--- src/app/shared/background.model.ts
+++ src/app/shared/background.model.ts
@@ -1,47 +1,58 @@
 import { Die } from './die.model';
 
-export class CharacterBackground {
+interface ICharacterBackground {
+    number: number;
+    name: string;
+    qualityChoices: string[];
+    qualityDiceSides: number[];
+    principleCategory: string;
+    powerSourceDiceSides: number[];
+    pageNumber: number;
+    blurb: string;
+    backgroundText: string;
+    diceText: string;
+}
+
+export class CharacterBackground implements ICharacterBackground {
     public number: number;
     public name: string;
-    public backgroundText: string;
-    public qualities: string[];
+    public qualityChoices: string[];
+    public qualityDiceSides: number[];
     public principleCategory: string;
-    public powerSourceDice: Die[] = [];
-    public diceText: string;
+    public powerSourceDiceSides: number[];
     public pageNumber: number;
-    public diceToAssign: Die[] = [];
+    public blurb: string;
+    public backgroundText: string;
+    public diceText: string;
+
+    public powerSourceDice: Die[];
+    public diceToAssign: Die[];
     public qualityDiceString: string;
 
-    constructor(backgroundData: {
-        num: number;
-        name: string;
-        qualities: string[];
-        quality_dice: number[];
-        principle: string;
-        power_source_dice: number[];
-        page: number;
-        blurb: string;
-        background_text: string;
-        dice_text: string;
-    }) {
-        this.number = backgroundData.num;
-        this.name = backgroundData.name;
-        this.backgroundText = backgroundData.background_text;
-        this.qualities = backgroundData.qualities;
-        this.principleCategory = backgroundData.principle;
-        this.diceText = backgroundData.dice_text;
-        this.pageNumber = backgroundData.page;
-        this.qualityDiceString = '';
+    constructor(backgroundData: ICharacterBackground) {
+        // this.number = backgroundData.num;
+        // this.name = backgroundData.name;
+        // this.backgroundText = backgroundData.background_text;
+        // this.qualities = backgroundData.qualities;
+        // this.principleCategory = backgroundData.principle;
+        // this.diceText = backgroundData.dice_text;
+        // this.pageNumber = backgroundData.page;
+        // this.qualityDiceString = '';
 
-        for (let dieSides of backgroundData.power_source_dice) {
-            let die = new Die(dieSides);
-            this.powerSourceDice.push(die);
-        }
+        Object.assign(this, backgroundData);
 
-        for (let dieSides of backgroundData.quality_dice) {
-            let die = new Die(dieSides);
-            this.diceToAssign.push(die);
-            this.qualityDiceString += die.name + ' ';
-        }
+        // for (let dieSides of backgroundData.power_source_dice) {
+        //     let die = new Die(dieSides);
+        //     this.powerSourceDice.push(die);
+        // }
+        this.powerSourceDice = this.powerSourceDiceSides.map((dieSides) => new Die(dieSides));
+
+        // for (let dieSides of backgroundData.quality_dice) {
+        //     let die = new Die(dieSides);
+        //     this.diceToAssign.push(die);
+        //     this.qualityDiceString += die.name + ' ';
+        // }
+        this.diceToAssign = this.qualityDiceSides.map((dieSides) => new Die(dieSides));
+        this.qualityDiceString = this.qualityDiceSides.map((dieSides) => 'D' + dieSides.toString()).join(' ');
     }
 }
