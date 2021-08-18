ef2e1b7f5ec2b3564af65410b88d8a0c421851f0
Daniel Scholler <shpaackle@gmail.com>
2021-08-18 17:15:13 -0400

    changed property action to actionType

--- src/app/shared/principle.model.ts
+++ src/app/shared/principle.model.ts
@@ -3,7 +3,7 @@ interface IPrinciple {
     duringRoleplayingText: string;
     minorTwist: string;
     majorTwist: string;
-    principleAbility: { icon: string; name: string; type: string; gameText: string };
+    principleAbility: { icon: string; name: string; actionType: string; gameText: string };
 }
 
 export class Principle implements IPrinciple {
@@ -11,7 +11,7 @@ export class Principle implements IPrinciple {
     public duringRoleplayingText: string;
     public minorTwist: string;
     public majorTwist: string;
-    public principleAbility: { icon: string; name: string; type: string; gameText: string };
+    public principleAbility: { icon: string; name: string; actionType: string; gameText: string };
 
     constructor(principleData: IPrinciple) {
         Object.assign(this, principleData);
