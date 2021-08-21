e20fa53f0632cad2ffee7e2762cbeed5fb033f00
Daniel Scholler <shpaackle@gmail.com>
2021-08-21 16:55:57 -0400

    moved code to quality-selection component

--- src/app/shared/quality-selection/quality-selection.component.ts
+++ src/app/shared/quality-selection/quality-selection.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-quality-selection',
-  templateUrl: './quality-selection.component.html',
-  styleUrls: ['./quality-selection.component.css']
+    selector: 'app-quality-selection',
+    templateUrl: './quality-selection.component.html',
+    styleUrls: ['./quality-selection.component.css'],
 })
 export class QualitySelectionComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
