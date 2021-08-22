446b178caed181b092c6a6896c9beb3716355b21
Daniel Scholler <shpaackle@gmail.com>
2021-08-22 18:43:38 -0400

    changed slelectedQuality to selectedQualities - made it an array - added index to ngFor loop - now passes index when event binding - now assigns quality at index to selection

--- src/app/shared/quality-selection/quality-selection.component.ts
+++ src/app/shared/quality-selection/quality-selection.component.ts
@@ -10,17 +10,20 @@ import { Die } from '../die.model';
 export class QualitySelectionComponent implements OnInit {
     @Input() selectedBackground: CharacterBackground;
 
-    selectedQuality: { die: Die; qualityName: string };
+    selectedQualities: { die: Die; qualityName: string }[];
 
     constructor() {}
 
     ngOnInit(): void {
-        this.selectedQuality = { die: new Die(), qualityName: 'Please select a quality' };
+        this.selectedQualities = this.selectedBackground.diceToAssign.map((die) => ({
+            die: new Die(die.sides),
+            qualityName: 'Please select a quality',
+        }));
     }
 
-    onQualityChoiceSelected(selectedQuality: { die: Die; qualityName: string }) {
+    onQualityChoiceSelected(selectedQuality: { die: Die; qualityName: string }, index: number) {
         console.log('onQualityChoiceSelected');
         console.log(selectedQuality);
-        this.selectedQuality = selectedQuality;
+        this.selectedQualities[index] = selectedQuality;
     }
 }
