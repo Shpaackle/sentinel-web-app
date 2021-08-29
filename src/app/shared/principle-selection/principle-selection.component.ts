8ddd92ace6d9730ca283bc886d4f801ac473c52b
Daniel Scholler <shpaackle@gmail.com>
2021-08-29 17:35:19 -0400

    tried changing how principle data handled broke the app going to try with reactive forms next

--- src/app/shared/principle-selection/principle-selection.component.ts
+++ src/app/shared/principle-selection/principle-selection.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { CharacterBackground } from '../background.model';
 import { Principle } from '../principle.model';
-import principles from '../../../assets/tables/principles.json';
+import * as principles from '../../../assets/tables/principles.json';
 import backgrounds from '../../../assets/tables/backgrounds.json';
 
 @Component({
@@ -9,18 +9,20 @@ import backgrounds from '../../../assets/tables/backgrounds.json';
     templateUrl: './principle-selection.component.html',
     styleUrls: ['./principle-selection.component.css'],
 })
-export class PrincipleSelectionComponent {
-    // implements OnInit {
-    @Input() selectedBackground: CharacterBackground;
+export class PrincipleSelectionComponent implements OnInit {
+    @Input() principleCategoryName: string;
 
+    principleOptions: Principle[] = [];
     selectedPrinciple: Principle = null;
     principleSelected = new EventEmitter<Principle>();
 
     constructor() {}
 
-    // ngOnInit(): void {
-    //     this.selectedBackground = new CharacterBackground(backgrounds['empty']);
-    // }
+    ngOnInit(): void {
+        const principlesDataFromCategory = principles[this.principleCategoryName];
+
+        this.principleOptions = principlesDataFromCategory.map((principleData) => new Principle(principleData));
+    }
 
     onPrincipleSelection(selectedElement: any) {
         console.log('selectedElement');
@@ -30,15 +32,15 @@ export class PrincipleSelectionComponent {
         this.principleSelected.emit(this.selectedPrinciple);
     }
 
-    getPrinciplesFromCategory(principleCategoryName: string): Principle[] {
-        const prinCatData = principles[principleCategoryName];
+    // getPrinciplesFromCategory(principleCategoryName: string): Principle[] {
+    //     const prinCatData = principles[principleCategoryName];
 
-        console.log(prinCatData);
-        let principlesList = [];
-        // const principlesList = prinCatData.map((principleData: Principle) => new Principle(principleData));
-        for (let principleName in prinCatData) {
-            principlesList.push(new Principle(prinCatData[principleName]));
-        }
-        return principlesList;
-    }
+    //     console.log(prinCatData);
+    //     let principlesList = [];
+    //     // const principlesList = prinCatData.map((principleData: Principle) => new Principle(principleData));
+    //     for (let principleName in prinCatData) {
+    //         principlesList.push(new Principle(prinCatData[principleName]));
+    //     }
+    //     return principlesList;
+    // }
 }
