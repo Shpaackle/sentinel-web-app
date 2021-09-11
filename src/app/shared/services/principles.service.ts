3d7ef344bf63fc342d9d0a61970795f7277a3165
Daniel Scholler <shpaackle@gmail.com>
2021-09-11 12:41:20 -0400

    commented import + removed unused testing code

--- src/app/shared/services/principles.service.ts
+++ src/app/shared/services/principles.service.ts
@@ -1,25 +1,20 @@
 import { Injectable } from '@angular/core';
 
-import { IPrinciple, Principle } from '../principle.model';
+import { Principle } from '../principle.model';
 
 import principlesData from '../../../assets/tables/principles-data.json';
-import { Observable, of } from 'rxjs';
+// import { Observable, of } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class PrinciplesService {
-    principles: IPrinciple[];
-
     private principlesByCategory: { [categoryName: string]: Principle[] } = {};
 
     constructor() {
         // in the constructor
         // build out the principles array from your json data
         // and any component that needs this data, grabs it from this service
-
-        // this.principlesByCategory['Esoteric'] = principlesData['Esoteric'];
-        // console.log(principlesData.Esoteric);
 
         for (let principleCategoryName in principlesData) {
             console.log(principleCategoryName);
@@ -30,16 +25,6 @@ export class PrinciplesService {
                 this.principlesByCategory[principleCategoryName].push(principle);
             }
         }
-
-        this.principles = [
-            new Principle({
-                name: 'test principle',
-                duringRoleplayingText: 'test',
-                minorTwist: 'minor twist',
-                majorTwist: 'major twist',
-                principleAbility: { icon: 'string', name: 'string', actionType: 'string', gameText: 'string' },
-            }),
-        ];
     }
 
     // getPrinciplesByCategoryName(categoryName: string): Observable<IPrinciple[]> {
