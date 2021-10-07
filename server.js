24886f24b1581f96ed54af2b16d293478cbede44
Daniel Scholler <shpaackle@gmail.com>
2021-10-06 20:55:12 -0400

    added string interpolation

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const onError = (error) => {
     if (error.syscall !== 'listen') {
         throw error;
     }
-    const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
+    const bind = typeof port === 'string' ? `pipe ${port}` : `port ${port}`;
     switch (error.code) {
         case 'EACCES':
             console.error(bind + ' requires elevated privileges');
