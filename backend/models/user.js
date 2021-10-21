7b3bb1c773e57687ff3388b7d8706aaf848d9a87
Daniel Scholler <shpaackle@gmail.com>
2021-10-21 19:14:09 -0400

    added schema comment

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,3 +7,26 @@ const userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
+
+/* 
+userName: String,
+memberID: String,
+firstName: String,
+lastName: String,
+email: String,
+avatar: String,
+twitter: String,
+instagram: String,
+discord: String,
+dateOfBirth: Date,
+password: String,
+charactersOwned: [{characterName: String, characterID: String}],
+savedSheets: [fileName: String, sheetID: String],
+superGroups: [{groupName: String, accessPrivilege: String, groupID: String}],
+villainsOwned: [{villainName: String, villainID: String}].
+campaignsCreated: [{cammpaignName: String, accessPrivilege: String, campaignID: String}]
+customContent: {
+    principles: [],
+    abilities: []
+}
+ */
