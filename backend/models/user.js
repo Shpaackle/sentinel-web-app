c5475fab337a1f5c5461fa7a182117c11ec0b469
Daniel Scholler <shpaackle@gmail.com>
2021-10-25 21:13:34 -0400

    removed required from name property

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    name: { type: String, required: true },
+    name: { type: String },
     characters: { type: String },
 });
 
