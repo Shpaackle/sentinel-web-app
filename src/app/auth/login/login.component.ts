3a7b63b67a145f80882f1cac32629d2b860b29c6
Daniel Scholler <shpaackle@gmail.com>
2021-11-21 17:31:17 -0500

    added userService to login & signup components

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { UserService } from 'src/app/shared/services/user.service';
+
 @Component({
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
     isLoading = false;
+
+    constructor(public userService: UserService) {}
 
     onLogin(form: NgForm) {
         console.log(form);
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { UserService } from 'src/app/shared/services/user.service';
+
 @Component({
     templateUrl: './signup.component.html',
     styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
     isLoading = false;
+
+    constructor(public userService: UserService) {}
 
     onSignup(form: NgForm) {
         console.log(form);
