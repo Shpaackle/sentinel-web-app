01477600a23230ffb50d91e4c86e90a75d618793
Daniel Scholler <shpaackle@gmail.com>
2021-12-08 21:04:42 -0500

    added mongoose-unique-validator removed code to check if User exists before saving

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
     username: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     userID: String,
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
