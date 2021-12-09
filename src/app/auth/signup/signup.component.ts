e3ab3dca2de3b0e22bc3869fc78a143c68e4ee85
Daniel Scholler <shpaackle@gmail.com>
2021-12-08 20:24:07 -0500

    started error handling for signup form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -9,14 +9,27 @@ import { UserService } from 'src/app/shared/services/user.service';
 })
 export class SignupComponent {
     isLoading = false;
+    signupError = false;
 
     constructor(public userService: UserService) {}
 
     onSignup(form: NgForm) {
         console.log(form);
 
+        console.log('form.value= ' + JSON.stringify(form.value));
+
         if (form.invalid) {
             return;
+        }
+
+        const username = form.value.username;
+        const email = form.value.email;
+        const password = form.value.password;
+
+        const responseMessage = this.userService.createUser(username, email, password);
+
+        if (responseMessage === 'User already exists') {
+            this.signupError = true;
         }
     }
 }
